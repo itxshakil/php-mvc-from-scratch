01af36ef7618286cd117ed1a3e7a47772e8b72f8
Shakil Alam <itxshakiil@gmail.com>
2020-04-04 14:20:51 +0530

    Added Route dispatcher

---
+++ App/Controller/Posts.php
@@ -0,0 +1,17 @@
+<?php class Posts
+{
+    public function index()
+    {
+        echo ' Hello from Index';
+    }
+
+    public function addNew()
+    {
+        echo 'Hello from add new method';
+    }
+
+    public function edit()
+    {
+        echo 'Hello from edit method';
+    }
+}
