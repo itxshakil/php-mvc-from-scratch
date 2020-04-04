1f42add77effe1828260fb9ae70b990c2659f228
Shakil Alam <itxshakiil@gmail.com>
2020-04-04 11:45:39 +0530

    added method for adding and getting routes

--- Core/Router.php
+++ Core/Router.php
@@ -1,5 +1,34 @@
 <?php
 
-class Router{
-    
+class Router
+{
+    /**
+     * Associated Array of route
+     *
+     * @var array
+     */
+    protected $routes = [];
+
+    /**
+     * Add a route to routing table
+     *
+     * @param string $route
+     * @param array $params (controller, action etc)
+     *
+     * @return void
+     */
+    public function add($route, $params)
+    {
+        $this->routes[$route] = $params;
+    }
+
+    /**
+     * Get all routes from routing table
+     *
+     * @return array
+     */
+    public function getRoutes()
+    {
+        return $this->routes;
+    }
 }
--- public/index.php
+++ public/index.php
@@ -1,7 +1,18 @@
 <?php
 
+/**                     
+ * Front Controller
+ */
 require './Core/Router.php';
 
 $router = new Router;
 
-echo get_class($router);
+//Add routes
+
+$router->add('', ['controller' => 'Home', 'action' => 'index']);
+$router->add('posts', ['controller' => 'Posts', 'action' => 'index']);
+$router->add('posts/new', ['controller' => 'Posts', 'action' => 'new']);
+
+echo '<pre>';
+var_dump($router->getRoutes());
+echo '</pre>';
