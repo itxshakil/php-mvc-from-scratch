12dafe8759d58776d2d3119e488b379b1b082d29
Shakil Alam <itxshakiil@gmail.com>
2020-04-05 09:41:07 +0530

    Added View

--- App/Controllers/Home.php
+++ App/Controllers/Home.php
@@ -3,21 +3,23 @@
 namespace App\Controllers;
 
 use Core\Controller;
+use Core\View;
 
 class Home extends Controller
 {
     public function indexAction()
     {
-        echo ' Hello from Home controllers index method';
+        View::render('Home/index.php');
+        // echo ' Hello from Home controllers index method';
     }
 
     protected function before()
     {
-        echo 'before';
+        // echo 'before';
     }
 
     protected function after()
     {
-        echo 'after';
+        // echo 'after';
     }
 }
---
+++ Core/View.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Core;
+
+class View
+{
+    /**
+     * Render a view file
+     *
+     * @param string $view The view file
+     *
+     * @return void
+     */
+    public static function render($view)
+    {
+        $file = "./App/Views/$view"; //  Relative to core directory
+        if (is_readable($file)) {
+            require $file;
+        }else{
+            echo "$file not found";
+        }
+    }
+}
