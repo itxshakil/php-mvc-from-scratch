df4e9ed3ae9450e1e65220c3da340e7dff175034
Shakil Alam <itxshakiil@gmail.com>
2020-04-05 11:38:28 +0530

    Configuration setting in separate config file

---
+++ App/Config.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App;
+
+ class Config
+ {
+     /**
+      * Database Configuration
+      */
+
+     const DB_HOST = 'localhost';
+     const DB_NAME = 'mvc';
+     const DB_USERNAME = 'root';
+     const DB_PASSWORD = '';
+ }
--- Core/Model.php
+++ Core/Model.php
@@ -2,6 +2,7 @@
 
 namespace Core;
 
+use App\Config;
 use PDO;
 use PDOException;
 
@@ -12,12 +13,9 @@ abstract class Model
         static $db = null;
 
         if ($db == null) {
-            $host = 'localhost';
-            $dbname = 'mvc';
-            $username = 'root';
-            $password = '';
             try {
-                $db = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
+                $dsn = 'mysql:host=' . Config::DB_HOST . ';dbname=' . Config::DB_NAME . ';charset=utf8';
+                $db = new PDO($dsn, Config::DB_USERNAME, Config::DB_PASSWORD);
             } catch (PDOException $e) {
                 echo $e->getMessage();
             }
